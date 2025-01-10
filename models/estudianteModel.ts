405c21d6b39862c56406fc987636c58d103c8794
Luciano Montilla <unonueve95@gmail.com>
2025-01-10 09:54:18 -0300

    Add model validations

--- models/estudianteModel.ts
+++ models/estudianteModel.ts
@@ -10,18 +10,27 @@ export interface IEstudiante {
 const estudianteSchema = new Schema<IEstudiante>({
     nombre: {
         type: String,
-        required: true,
+        required: [true, "⚠️ Nombre es un campo requerido"],
     },
     apellido: {
         type: String,
-        required: true,
+        required: [true, "⚠️ Apellido es un campo requerido"],
     },
     email: {
         type: String,
         required: true,
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, "⚠️ Formato de email inválido"],
     },
     cursos: [
-        { type: String, enum: ["Matemática", "Historia", "Ciencias", "Arte"], required: true },
+        {
+            type: String,
+            enum: {
+                values: ["Matemática", "Historia", "Ciencias", "Arte"],
+                message: "⚠️ {VALUE} no es un valor permitido",
+            },
+            required: true,
+        },
     ],
 });
 
